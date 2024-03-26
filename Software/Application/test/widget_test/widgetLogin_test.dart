1b68f3445b0f916f47b256550f62802858cba107
Filipe Samuel Faria Loureiro <a23503@alunos.ipca.pt>
2024-03-26 09:53:10 +0000

    Remove unused test files and add new widget tests

--- Software/Application/test/widget_test/widgetLogin_test.dart
+++ Software/Application/test/widget_test/widgetLogin_test.dart
@@ -12,33 +12,22 @@ class MockAuthCubit extends Mock implements AuthCubit {}
 void main() {
   group('LoginUI Widget Tests', () {
     testWidgets('Login with valid credentials', (WidgetTester tester) async {
-      // Mock CubitFactory
       final MockAuthCubit authCubit = MockAuthCubit();
 
-      // Pump the LoginUI widget with MaterialApp
       await tester.pumpWidget(MaterialApp(
         home: LoginUI()));
 
       expect(find.text('Login'), findsOneWidget);
 
-      // Define valid email and password
-      final String validEmail = 'test@example.com';
-      final String validPassword = 'password';
-
-      // Enter email and password using TextEditingController
       //await tester.enterText(find.byKey(ValueKey('email_field')), validEmail);
       //await tester.enterText(find.byKey(ValueKey('password_field')), validPassword);
 
-      // Tap on the login button
       //await tester.tap(find.byKey(ValueKey('login_button')));
 
-      // Wait for the UI to settle
       //await tester.pumpAndSettle();
 
-      // Verify if the loginWithEmailAndPassword method was called with the correct arguments
       //verify(authCubit.loginWithEmailAndPassword(false, validEmail, validPassword)).called(1);
 
-      // Verify if the user is navigated to the home screen upon successful login
       expect(find.byKey(ValueKey('home_screen')), findsOneWidget);
     });
   });
